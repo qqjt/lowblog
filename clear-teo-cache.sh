650cf05bb42ad94cd930ad660fa67a12ccc995ec
qqjt <itkid@live.com>
2024-11-14 15:43:04 +0800

    debug

--- clear-teo-cache.sh
+++ clear-teo-cache.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # https://forum.gitlab.com/t/ci-cd-pipeline-get-list-of-changed-files/26847/2
 # shellcheck disable=SC2207
-files=($(git diff-tree --no-commit-id --name-only -r $1))
+files=($(git diff-tree --no-commit-id --name-only -r $CI_COMMIT_SHA))
 # Loop through the list
 for file in "${files[@]}"; do
     echo "$file"
 done
+export BASE_URL=$BASE_URL
+export TENCENTCLOUD_SECRET_ID=$TENCENTCLOUD_SECRET_ID
+export TENCENTCLOUD_SECRET_KEY=$TENCENTCLOUD_SECRET_KEY
+export TENCENTCLOUD_TEO_ZONE_ID=$TENCENTCLOUD_TEO_ZONE_ID
 npm run teo -- "${files[@]}"
